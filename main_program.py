d959fd2c95906b31ecd92d95c1629695380fa821
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-04-11 16:07:41 -0400

    Update and rename multiprocessing.py to main_program.py

--- main_program.py
+++ main_program.py
@@ -9,22 +9,22 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-
+## ACTIVE LOW! ##
 GPIO.setwarnings(False) # turn off warnings
 GPIO.setmode(GPIO.BCM) # use GPIO numbers to specify inputs
-GPIO.setup(2, GPIO.OUT, initial=GPIO.LOW) # GPIO2 = soleniod
-GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW) # GPIO3 = acid
-GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW) # GPIO4 = base
+GPIO.setup(2, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = soleniod
+GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = acid
+GPIO.setup(4, GPIO.OUT, initial=GPIO.HIGH) # GPIO4 = base
 GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # GPIO5 = water
 GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW) # GPIO6 = nuterients
 GPIO.setup(7, GPIO.IN) # GPIO7 = read whether big pump is on or not
 def mist_cycle():
     try:
         while True:
-            GPIO.output(2, GPIO.LOW)
-            sleep(300)
-            GPIO.output(2, GPIO.HIGH)
+            GPIO.output(2, GPIO.LOW) # TURN MIST ON
             sleep(5)
+            GPIO.output(2, GPIO.HIGH) # TURN MIST OFF
+            sleep(300)
     except KeyboardInterrupt:
         GPIO.cleanup()
     
@@ -35,21 +35,21 @@ def res_maintain():
         print("failed to find serial")
     s = [0]
     while True:
-        read_serial = ser.readline() ## find the format so you can just get numbers
+        read_serial = str(ser.readline()) ## find the format so you can just get numbers
         print(read_serial)
         # or modify ph sketch 
         # Should I pipe data from this function to a different one?
         ##### FOR NOW #####
-        ph_balance = int(read_serial[read_serial.index('pH:')+3:read_serial.index('pH:')+7])
+        ph_balance = int(read_serial[read_serial.indexOf('pH:')+3:read_serial.indexOf('pH:')+7])
         if ph_balance > 6.3:
-            GPIO.output(3, GPIO.HIGH)
-            sleep(5)
             GPIO.output(3, GPIO.LOW)
+            sleep(5)
+            GPIO.output(3, GPIO.HIGH)
             sleep(300)
         elif ph_balance < 5.3:
-            GPIO.output(4, GPIO.HIGH)
-            sleep(5)
             GPIO.output(4, GPIO.LOW)
+            sleep(5)
+            GPIO.output(4, GPIO.HIGH)
             sleep(300)
 
 
