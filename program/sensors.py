3e2f0ed1528f4c91944ec5a0367c9f193ebf9799
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 01:40:01 -0400

    Update sensors.py
    changed function name. Changed from open to with open. Added ec to sensed data.

--- program/sensors.py
+++ program/sensors.py
@@ -1,32 +1,28 @@
-def res_maintain(none):
-    # next, right ph to a file 
-    ph_log = open("ph_log", "w")
+def sensor_data(none):
+    inst_time = datetime.datetime.now()
     blank = none
     try:
         ser = serial.Serial('/dev/ttyACM0',9600)
     except:
         print("failed to find serial")
-    s = [0]
     while True:
-        read_serial = str(ser.readline()) ## find the format so you can just get numbers
-        # or modify ph sketch 
-        # Should I pipe data from this function to a different one?
-        ##### FOR NOW #####
-        if "pH" in read_serial:
-            read_serial = read_serial.split("#")
-            ph_balance = float(re.sub('[^0-9.]', '', read_serial[1]))
-            voltage = float(re.sub('[0-9.]', '', read_serial[0]))
-            if ph_balance > 6.3:
-                ph_log.write(f"pH is too high; pH: {ph_balance}; voltage: {voltage}; {datetime.datetime.now()}")
-                GPIO.output(3, GPIO.LOW)
-                sleep(5)
-                GPIO.output(3, GPIO.HIGH)
-                sleep(5)
-            elif ph_balance < 5.3:
-                ph_log.write(f"pH is too low; pH: {ph_balance}; voltage: {voltage}; {datetime.datetime.now()}")
-                GPIO.output(4, GPIO.LOW)
-                sleep(5)
-                GPIO.output(4, GPIO.HIGH)
-                sleep(5)
-        else:
-            pass
+        read_serial = str(ser.readline(), 'UTF-8') # readline should hang until a new line is sent
+        read_serial = read_serial.split("#")
+        voltage = float(read_serial[0])
+        ph_balance = float(read_serial[1])
+        ec = float(read_serial[2])
+        with open(f"ph_log_{inst_time}", "w") as log:
+            log.write(f"pH: {ph_balance}; voltage: {voltage}; ec: {ec}; time: {datetime.datetime.now()}")
+        # This should be made into it's own process so the sleeps don't stop data collection
+        if ph_balance > 6.3:
+            print("ph high")
+            GPIO.output(3, GPIO.LOW)
+            sleep(5)
+            GPIO.output(3, GPIO.HIGH)
+            sleep(5)
+        elif ph_balance < 5.3:
+            print("ph low")
+            GPIO.output(4, GPIO.LOW)
+            sleep(5)
+            GPIO.output(4, GPIO.HIGH)
+            sleep(5)
