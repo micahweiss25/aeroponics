60541981ffb0879e6007e8ddd01beb49840c5a5d
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:45:23 -0400

    Update test_peristaltic_pumps.py
    finished script to test pumps

--- test_peristaltic_pumps.py
+++ test_peristaltic_pumps.py
@@ -1,1 +1,17 @@
-    GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = base
+import time
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = base
+GPIO.setup(27, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = acid
+while True:
+    print("base ON")
+    GPIO.output(3, GPIO.LOW)
+    sleep(2)
+    GPIO.output(3, GPIO.HIGH)
+    print("acid ON")
+    GPIO.output(27, GPIO.LOW)
+    sleep(2)
+    GPIO.output(27, GPIO.HIGH)
