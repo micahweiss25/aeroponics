10b704ae52b91dcfc91092db3103a14030254253
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:37:42 -0400

    Update connect_gpio.py
    removed temp sensor

--- program/connect_gpio.py
+++ program/connect_gpio.py
@@ -7,6 +7,6 @@ def make_connection():
     #GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) # GPIO17 = nuterients 4th relay
     #GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # GPIO5 = water 
     #GPIO.setup(7, GPIO.IN) # GPIO7 = read whether big pump is on or not
-    GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # GPIO4 = temp sensor 
+    #GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # GPIO4 = temp sensor 
     # This one confuses me. IDK why it has to be GPIO4 to read this sensor.
     GPIO.setup(27, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = acid
