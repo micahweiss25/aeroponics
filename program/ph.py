f96778e24ca8ed3645a5d8644f9d02bc68672854
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:22:26 -0400

    Update ph.py
    increased the upper and lower bound for ph

--- program/ph.py
+++ program/ph.py
@@ -1,12 +1,12 @@
 def ph_cycle(None):
   blank = None # don't think this is needed
-  if ph_balance > 6.3:
+  if ph_balance > 7:
       print("ph high")
       GPIO.output(3, GPIO.LOW)
       sleep(5)
       GPIO.output(3, GPIO.HIGH)
       sleep(600)
-  elif ph_balance < 5.3:
+  elif ph_balance < 5:
       print("ph low")
       GPIO.output(4, GPIO.LOW)
       sleep(5)
