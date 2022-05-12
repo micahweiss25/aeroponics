48a890a5d6758ec2902efe50e66b7dee0da126a6
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:14:55 -0400

    Update misters.py
    changed file open to use with open()

--- program/misters.py
+++ program/misters.py
@@ -1,15 +1,14 @@
-# need to import sleep and GPIO
 def mist_cycle(none):
-    mist_log = open("mist_log", "w")
     blank = none
     count = 0
-    try:
-        while True:
-            mist_log.write(f"mist cycle {count}")
-            GPIO.output(2, GPIO.LOW) # TURN MIST ON
-            sleep(5)
-            GPIO.output(2, GPIO.HIGH) # TURN MIST OFF
-            sleep(5)
-            count += 1
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+    with open(f"mist_log_{datetime.datetime.now()}", "w")
+        try:
+            while True:
+                mist_log.write(f"mist cycle {count}; time: {datetime.datetime.now()}")
+                GPIO.output(2, GPIO.LOW) # TURN MIST ON
+                sleep(5)
+                GPIO.output(2, GPIO.HIGH) # TURN MIST OFF
+                sleep(5)
+                count += 1
+        except KeyboardInterrupt:
+            GPIO.cleanup()
