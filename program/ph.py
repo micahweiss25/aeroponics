07c504d4f1c2ad7ebb13c942939131d8a21a7d91
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:08:23 -0400

    Create ph.py
    created a separate script manage ph

---
+++ program/ph.py
@@ -0,0 +1,13 @@
+def ph_pumps():
+  if ph_balance > 6.3:
+      print("ph high")
+      GPIO.output(3, GPIO.LOW)
+      sleep(5)
+      GPIO.output(3, GPIO.HIGH)
+      sleep(5)
+  elif ph_balance < 5.3:
+      print("ph low")
+      GPIO.output(4, GPIO.LOW)
+      sleep(5)
+      GPIO.output(4, GPIO.HIGH)
+      sleep(5)
