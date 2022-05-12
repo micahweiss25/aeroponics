fa0e3c60e635c7bd578b291858ad37f8c8900a4c
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:47:54 -0400

    Update connect_gpio.py
    turned pins to vars

--- program/connect_gpio.py
+++ program/connect_gpio.py
@@ -1,12 +1,15 @@
 def make_connection():
     ## ACTIVE LOW! ##
+    ACID = 27
+    BASE = 3
+    SOLENIOD = 2
     GPIO.setwarnings(False) # turn off warnings
     GPIO.setmode(GPIO.BCM) # use GPIO numbers to specify inputs
-    GPIO.setup(2, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = soleniod
-    GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = base
+    GPIO.setup(SOLENIOD, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = soleniod
+    GPIO.setup(BASE, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = base
     #GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) # GPIO17 = nuterients 4th relay
     #GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # GPIO5 = water 
     #GPIO.setup(7, GPIO.IN) # GPIO7 = read whether big pump is on or not
     #GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # GPIO4 = temp sensor 
     # This one confuses me. IDK why it has to be GPIO4 to read this sensor.
-    GPIO.setup(27, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = acid
+    GPIO.setup(ACID, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = acid
