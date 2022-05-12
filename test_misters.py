f7428fa68fcdb0686fca09b90b38df967416c6e0
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-12 02:26:17 -0400

    Create test_misters.py
    created a script to test the misters

---
+++ test_misters.py
@@ -0,0 +1,11 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+    
+GPIO.setup(2, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = soleniod
+while True:
+  GPIO.output(2, GPIO.LOW) # TURN MIST ON
+  sleep(3)
+  GPIO.output(2, GPIO.HIGH) # TURN MIST OFF
+  sleep(3)
