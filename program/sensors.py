0ec0bde0f623ca086142d695888979d8460bc787
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-21 18:37:17 -0400

    Update sensors.py

--- program/sensors.py
+++ program/sensors.py
@@ -1,11 +1,21 @@
+import csv
+
+fields = ['pH', 'volt', 'ec', 'temp', 'time']
+
 def sensor_data(none):
     inst_time = datetime.datetime.now()
+    filename = f"ph_log_{inst_time}.csv"
+    with open("filename", "w") as log:
+        csvlog = csv.writer(log)
+        csvlog.writerow(fields)
+   
     blank = none
     try:
         ser = serial.Serial('/dev/ttyACM0',9600)
     except:
         print("failed to find serial")
-    # the first readline sometimes happens midline, so I just get rid of it
+        
+    # the first readline sometimes only receives part of the sensor data line, so I just get rid of it
     toss = ser.readline()
     while True:
         read_serial = str(ser.readline(), 'UTF-8') # readline should hang until a new line is sent
@@ -14,7 +24,10 @@ def sensor_data(none):
         ph_balance = float(read_serial[1])
         ec = float(read_serial[2])
         temp = float(read_serial[3])
-        with open(f"ph_log_{inst_time}", "w") as log:
-            log.write(f"pH: {ph_balance}; voltage: {voltage}; ec: {ec}; temp: {temp}; time: {datetime.datetime.now()}")
+        time = datetime.datetime.now()
+        data = [ph_balance, voltage, ec, temp, time]
+        with open("filename", "a") as log:
+            log.writerow(data)
+            #log.write(f"pH: {ph_balance}; voltage: {voltage}; ec: {ec}; temp: {temp}; time: {datetime.datetime.now()}")
         # This should be made into it's own process so the sleeps don't stop data collection
 
