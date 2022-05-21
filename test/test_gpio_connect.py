ce08b3c27bfc7c4bcbf91e4127fd59e7d1d2d586
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-05-21 16:11:14 -0400

    Update test_gpio_connect.py
    update

--- test/test_gpio_connect.py
+++ test/test_gpio_connect.py
@@ -1,15 +1,20 @@
-ACID = 27
+ACID = 2
 BASE = 3
-SOLENIOD = 2
+NUTRIENT = 4
+WATER = 17
+SOLENIOD = 27
+    
 def make_connection():
     ## ACTIVE LOW! ##
     GPIO.setwarnings(False) # turn off warnings
     GPIO.setmode(GPIO.BCM) # use GPIO numbers to specify inputs
-    GPIO.setup(SOLENIOD, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = soleniod
-    GPIO.setup(BASE, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = base
-    #GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) # GPIO17 = nuterients 4th relay
+    GPIO.setup(SOLENIOD, GPIO.OUT, initial=GPIO.HIGH) # GPIO2 = ACID
+    GPIO.setup(BASE, GPIO.OUT, initial=GPIO.HIGH) # GPIO3 = BASE
+    GPIO.setup(NUTRIENT, GPIO.OUT, initial=GPIO.HIGH) # GPIO4 = NUTRIENT
+    GPIO.setup(WATER, GPIO.OUT, initial=GPIO.HIGH) # GPIO17 = WATER
+    GPIO.setup(SOLENIOD, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = SOLENIOD
     #GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # GPIO5 = water 
     #GPIO.setup(7, GPIO.IN) # GPIO7 = read whether big pump is on or not
     #GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # GPIO4 = temp sensor 
     # This one confuses me. IDK why it has to be GPIO4 to read this sensor.
-    GPIO.setup(ACID, GPIO.OUT, initial=GPIO.HIGH) # GPIO27 = acid
+
