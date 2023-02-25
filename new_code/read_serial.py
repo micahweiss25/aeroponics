2b430965b4c8bfa8ead7b7213fae919eb4ecfbe3
micah <m.d.weiss25@gmail.com>
2023-02-25 14:32:35 -0500

    commit

--- new_code/read_serial.py
+++ new_code/read_serial.py
@@ -1,5 +1,16 @@
 import serial
+import time
+import logging
+from datetime import datetime
+
+f = open(r"./logs/sensor_data", "a")
+
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 while True:
-    print(ser.readline())
+    data = ser.readline().decode("utf-8").replace("#", ",").replace("\n", "") + "," +  datetime.now().strftime("%H:%M:%S") + "\n"
+    f.write(data)
+    #logging.info(data)
+    time.sleep(60)
+    
+
