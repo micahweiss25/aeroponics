0bd8b5f601acd7a4f1bbb582deafe690d26938e6
micahweiss25 <m.d.weiss25@gmail.com>
2023-02-24 20:03:36 -0500

    read serial

--- read_serial_send2_database.py
+++ read_serial_send2_database.py
@@ -17,13 +17,13 @@ db_cursor = db_connection.cursor()
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 while True:
     # read data from serial port
-    data = ser.readline()
-    print(data)
+    data = ser.readline().split
+    print(f"{voltage: data[0]}, ph: {data[1]}, tds: {data[2]}, temp: {data[3]}")
 
     # I STILL NEED TO INTERPRET THE DATA BEFORE ENTERING INTO DATABASE
 
     # write data to database
-    db_cursor.execute("INSERT INTO aero_data (data) VALUES (%s)", (data,))
+    db_cursor.execute("INSERT INTO aero_data (volt, ph, tds, temp) VALUES (%s, %s, %s, %s)", (data[0],data[1],data[2],data[3]))
     db_connection.commit()
     print(db_connection.rowcount, "record inserted.")
     
