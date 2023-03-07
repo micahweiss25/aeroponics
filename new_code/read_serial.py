f49dbdac1283b04bf537be9f018e747cbf0f5666
micah <m.d.weiss25@gmail.com>
2023-03-07 11:31:53 -0500

    improved sensor logging script

--- new_code/read_serial.py
+++ new_code/read_serial.py
@@ -3,14 +3,19 @@ import time
 import logging
 from datetime import datetime
 
-f = open(r"./logs/sensor_data", "a")
 
+last_time = time.time()
+data = ""
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 while True:
-    data = ser.readline().decode("utf-8").replace("#", ",").replace("\n", "") + "," +  datetime.now().strftime("%H:%M:%S") + "\n"
-    f.write(data)
-    #logging.info(data)
-    time.sleep(60)
+    if time.time() - last_time > 15:
+        last_time = time.time()
+        f = open(r"./logs/sensor_data", "a")
+        f.write(data)
+        f.close()
+        data = ""
+    data += ser.readline().decode("utf-8").replace("#", ",").replace("\n", "") + "," +  datetime.now().strftime("%H:%M:%S") + "\n"
+    time.sleep(1)
     
 
